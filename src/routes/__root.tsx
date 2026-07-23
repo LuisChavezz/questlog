ce29bc11830cb5ba344c7a9be74bfcc3fca0c1a0
LuisChavezz <ing.luischavezz@gmail.com>
2026-07-23 15:18:09 -0600

    refactor(routes): use # alias for not-found-page import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,7 +8,7 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 import { ThemeProvider } from '../components/providers/theme-provider'
-import { NotFoundPage } from '../components/not-found-page'
+import { NotFoundPage } from '#/components/not-found-page'
 
 import appCss from '../styles.css?url'
 
